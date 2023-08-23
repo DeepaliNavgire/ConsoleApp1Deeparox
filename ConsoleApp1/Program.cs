67910c69e2770c943022d9a441f3f680034b860b
DN <142329844+DeepaliNavgire@users.noreply.github.com>
2023-08-23 15:06:26 +0530

    Added for loop
    into program.cs file from deeparox

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            for(i=1;i<=100;i++)
+            {
             console.writeline("hello everyone");
+            }
         }
     }
 }
